fb4e690c1177c0d6c70fe00e2decbc1e37498a2a
mcakunz <matheus.kunz05@gmail.com>
2026-04-07 20:08:44 -0300

    feat: componente generico para formularios

---
+++ app/components/ui/RequiredSymbol.tsx
@@ -0,0 +1,10 @@
+export function RequiredSymbol() {
+    return (
+        <span
+            className="text-red-500 ml-0.5"
+            title="Campo obrigatório"
+        >
+            *
+        </span>
+    )
+}
